c38de504107d0e79084925f5c1eb1662a32ba0be
David <dave28221.@hotmail.com>
2025-04-16 15:36:42 +0800

    4325346

--- config/database.js
+++ config/database.js
@@ -15,17 +15,18 @@ module.exports = ({ env }) => {
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
-    postgres: {  // PostgreSQL connection setup
+    postgres: {
       connection: {
-        host: env('DATABASE_HOST', 'postgresql+ssh://ploi@157.180.76.148/ploi@127.0.0.1/strapi_production?name=strapi-project-manual-setup&usePrivateKey=true'),  // Use the correct host for your database (e.g., IP address or hostname)
-        port: env.int('DATABASE_PORT', 5432),  // Default PostgreSQL port
+        host: env('DATABASE_HOST', '157.180.76.148'),
+        port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi_production'),
-        user: env('DATABASE_USERNAME', 'David'),  // Your PostgreSQL username
-        password: env('DATABASE_PASSWORD', 'llmstrapi'),  // Your PostgreSQL password
-        ssl: env.bool('DATABASE_SSL', true),  // Set to `true` for SSL connections in production
+        user: env('DATABASE_USERNAME', 'David'),
+        password: env('DATABASE_PASSWORD', 'llmstrapi'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
+
   };
 
   return {
