fa82ba47f6d85d0c5a5d373e531b3f45aaaaa2ac
David <dave28221.@hotmail.com>
2025-04-16 16:21:46 +0800

    43254342634

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,21 @@
-// ecosystem.config.js
 module.exports = {
-    apps: [
-      {
-        name: 'strapi',
-        script: 'npm',
-        args: 'start',
-        env: {
-          NODE_ENV: 'production',
-        },
+  apps: [
+    {
+      name: 'strapi',
+      script: 'npm',
+      args: 'start',
+      env: {
+        NODE_ENV: 'production',
+        DATABASE_HOST: '157.180.76.148',
+        DATABASE_PORT: '5432',
+        DATABASE_NAME: 'strapi_production',
+        DATABASE_USERNAME: 'David',
+        DATABASE_PASSWORD: 'llmstrapi',
+        DATABASE_SSL: 'true',
+        JWT_SECRET: 'nB68jUfziXuNXvCGHSl+7g==',
+        STRAPI_ADMIN_BACKEND_URL: 'https://liftlegalwp.com.au/admin',
+        STRAPI_TELEMETRY_DISABLED: 'true',
       },
-    ],
-  };
-  
+    },
+  ],
+};
