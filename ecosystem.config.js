c372635afaae8375d901c8d579d9978f440487e9
David <dave28221.@hotmail.com>
2025-04-16 12:35:53 +0800

    423w53245

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+// ecosystem.config.js
+module.exports = {
+    apps: [
+      {
+        name: 'strapi',
+        script: 'npm',
+        args: 'start',
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+    ],
+  };
+  
