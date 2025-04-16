d29ac59eb12ec655fa24b1de409c7c6c2bbcfb60
David <dave28221.@hotmail.com>
2025-04-16 15:17:25 +0800

    43253465346

--- config/database.js
+++ config/database.js
@@ -17,13 +17,8 @@ module.exports = ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env('postgresql+ssh://ploi@157.180.76.148/ploi@127.0.0.1/strapi_production?name=strapi-project-manual-setup&usePrivateKey=true'),
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi_production'),
-        user: env('DATABASE_USERNAME', 'David'),
-        password: env('DATABASE_PASSWORD', 'llm202'),
-        ssl: false, // 👈 disable SSL for local PostgreSQL
+        connectionString: env('DATABASE_URL', 'postgresql+ssh://ploi@157.180.76.148/ploi@127.0.0.1/strapi_production?name=strapi-project-manual-setup&usePrivateKey=true'),
+        ssl: false, // 👈 disable SSL for local PostgreSQL, enable for production
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,14 @@
+// ecosystem.config.js
 module.exports = {
-  apps: [
-    {
-      name: 'liftlegalwp.com.au',  // Ensure this matches what you're using in PM2
-      script: './server.js',     // Adjust this if your entry file is different
-      env: {
-        NODE_ENV: 'development',
+    apps: [
+      {
+        name: 'strapi',
+        script: 'npm',
+        args: 'start',
+        env: {
+          NODE_ENV: 'production',
+        },
       },
-      env_production: {
-        NODE_ENV: 'production',
-      },
-    },
-  ],
-};
+    ],
+  };
+  
