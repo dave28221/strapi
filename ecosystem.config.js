f957375b3c864e33a1ed9cd8513ad6b08dc340e3
David <dave28221.@hotmail.com>
2025-04-16 15:15:05 +0800

    4e34325

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,14 @@
-// ecosystem.config.js
 module.exports = {
-    apps: [
-      {
-        name: 'strapi',
-        script: 'npm',
-        args: 'start',
-        env: {
-          NODE_ENV: 'production',
-        },
+  apps: [
+    {
+      name: 'liftlegalwpcomau',  // Ensure this matches what you're using in PM2
+      script: './server.js',     // Adjust this if your entry file is different
+      env: {
+        NODE_ENV: 'development',
       },
-    ],
-  };
-  
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
