6cd986547573c6ee7a2eec8d73cc183e4b0c5562
David <dave28221.@hotmail.com>
2025-04-16 16:45:12 +0800

    4325346

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,13 +8,13 @@ module.exports = {
         NODE_ENV: 'development',  // Default environment for development
       },
       env_production: {
-        NODE_ENV: 'production',  // Environment configuration for production
+        NODE_ENV: 'production',  // Environment configuration for production|
         DATABASE_HOST: '157.180.76.148', // Public IP of your PostgreSQL database
         DATABASE_PORT: '5432',
         DATABASE_NAME: 'strapi_production',
         DATABASE_USERNAME: 'David',
         DATABASE_PASSWORD: 'llmstrapi',
-        DATABASE_SSL: 'true', // Set true if you want to use SSL
+        DATABASE_SSL: 'false', // Set true if you want to use SSL
         JWT_SECRET: 'nB68jUfziXuNXvCGHSl+7g==',
         STRAPI_ADMIN_BACKEND_URL: 'https://liftlegalwp.com.au/admin',
         STRAPI_TELEMETRY_DISABLED: 'true',
