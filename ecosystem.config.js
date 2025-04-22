a18ed367491a153dd4793efd047cc65bc1fbd0bd
David <dave28221.@hotmail.com>
2025-04-22 15:32:38 +0800

    4325346 port change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,8 +12,8 @@ module.exports = {
         DATABASE_HOST: '127.0.0.1', // Public IP of your PostgreSQL database
         DATABASE_PORT: '5432',
         DATABASE_NAME: 'strapi_production',
-        DATABASE_USERNAME: 'David',
-        DATABASE_PASSWORD: 'llm202',
+        DATABASE_USERNAME: ' ploi',
+        DATABASE_PASSWORD: 'vpcxf9Qzi506u8rpDbD0',
         DATABASE_SSL: 'false', // Set true if you want to use SSL
         JWT_SECRET: 'nB68jUfziXuNXvCGHSl+7g==',
         STRAPI_ADMIN_BACKEND_URL: 'https://liftlegalwp.com.au/admin',
