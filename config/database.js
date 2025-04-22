81fbe6e5b596816f013a4d0b3dfe7c323ebd0a9e
David <dave28221.@hotmail.com>
2025-04-22 15:57:49 +0800

    436545654567

--- config/database.js
+++ config/database.js
@@ -6,11 +6,11 @@ module.exports = ({ env }) => {
   const connections = {
     postgres: {
       connection: {
-        host: env('DATABASE_HOST', '157.180.76.148'),
+        host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi_production'),
-        user: env('DATABASE_USERNAME', 'David'),
-        password: env('DATABASE_PASSWORD', 'llmstrapi'),
+        user: env('DATABASE_USERNAME', 'ploi'),
+        password: env('DATABASE_PASSWORD', 'vpcxf9Qzi506u8rpDbD0'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
--- config/server.js
+++ config/server.js
@@ -1,10 +1,13 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+module.exports = ({ env }) => {
+  console.log('Server Port:', env.int('PORT', 1338));  // Add this line to debug
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1338),
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+    webhooks: {
+      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+  };
+};
