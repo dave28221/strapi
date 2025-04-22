3ca75a9bfbcb61878826efc4d757c425b5c1985c
David <dave28221.@hotmail.com>
2025-04-22 12:05:30 +0800

    423534667

--- config/database.js
+++ config/database.js
@@ -4,17 +4,6 @@ module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
 
   const connections = {
-    mysql: {
-      connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi_production'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: false,
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
     postgres: {
       connection: {
         host: env('DATABASE_HOST', '157.180.76.148'),
