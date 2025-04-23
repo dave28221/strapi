caef8020d02446b8489719ca39777d542d7631f7
David <dave28221.@hotmail.com>
2025-04-23 11:28:21 +0800

    4235346

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => {
   console.log('Server Port:', env.int('PORT', 1338));  // Add this line to debug
-  return {
-    url: '/admin',
+  return {  
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1338),
     app: {
