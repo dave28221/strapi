250c547c8b0907f1b421f5d404d2d49ed5f96310
David <65214573+dave28221@users.noreply.github.com>
2025-05-28 12:03:49 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,12 +1,10 @@
-const path = require('path');
-
 module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'postgres');
 
   const connections = {
     postgres: {
       connection: {
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'db'),            // default to 'db' in Docker
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapiuser'),
@@ -15,7 +13,6 @@ module.exports = ({ env }) => {
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
-
   };
 
   return {
