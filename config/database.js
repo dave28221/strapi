dd38ae49c5ae058588a49e4331d7305a9ce5c97e
David <65214573+dave28221@users.noreply.github.com>
2025-05-28 10:29:09 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -6,11 +6,11 @@ module.exports = ({ env }) => {
   const connections = {
     postgres: {
       connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', '157.180.76.148'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi_production'),
-        user: env('DATABASE_USERNAME', 'ploi'),
-        password: env('DATABASE_PASSWORD', 'vpcxf9Qzi506u8rpDbD0'),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapiuser'),
+        password: env('DATABASE_PASSWORD', 'tkNHJhXLNDS5GcRbH61DDSkQ6chE17SHgQprn8ghlsq1P6hUQlQTydF11cQajPt2'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
