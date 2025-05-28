4d4e0fbcc7485f7eb73e76007bf38575d1857ed5
David <65214573+dave28221@users.noreply.github.com>
2025-05-28 13:55:13 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => {
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'postgresql-database'),
         user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'L39KNym5x4wmrwwqIadzI2DN7gFL0W6uq5F5T8iNNxwZGiloyffx58muv4vSekf2),
+        password: env('DATABASE_PASSWORD', 'L39KNym5x4wmrwwqIadzI2DN7gFL0W6uq5F5T8iNNxwZGiloyffx58muv4vSekf2'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
