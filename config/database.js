270bc62c7d6bf01b5c8974ecfc8ccf7ba610d139
David <65214573+dave28221@users.noreply.github.com>
2025-05-28 15:50:16 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,15 +3,19 @@ module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'postgres');
 
   const connections = {
-    postgres: {
-      connection: {
-      host: env('DATABASE_HOST', 'postgresql-database'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'postgres'),
+module.exports = ({ env }) => ({
+  connection: {
+    client: env('DATABASE_CLIENT', 'postgres'),
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', false),
-      },
+    },
+  },
+});
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
   };
