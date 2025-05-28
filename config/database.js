9bc74b4efd3834a71e73839ea6d1fbcc02210501
David <65214573+dave28221@users.noreply.github.com>
2025-05-28 15:57:05 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,3 @@
-
-module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
-
-  const connections = {
 module.exports = ({ env }) => ({
   connection: {
     client: env('DATABASE_CLIENT', 'postgres'),
@@ -14,17 +9,10 @@ module.exports = ({ env }) => ({
       password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', false),
     },
+    pool: {
+      min: env.int('DATABASE_POOL_MIN', 2),
+      max: env.int('DATABASE_POOL_MAX', 10),
+    },
+    acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
   },
 });
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-  };
-
-  return {
-    connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
-    },
-  };
-};
