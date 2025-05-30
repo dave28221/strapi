428435f78f5fc1d8bbfbbebd741ddf569cf4e3ce
David <65214573+dave28221@users.noreply.github.com>
2025-05-30 14:11:38 +0800

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,11 @@
-module.exports = ({ env }) => ({
-  'users-permissions': {
-    config: {
-      jwtSecret: env('JWT_SECRET'),
+module.exports = ({ env }) => {
+  const jwtSecret = env('JWT_SECRET');
+  console.log('JWT_SECRET loaded:', jwtSecret ? 'YES' : 'NO');
+  return {
+    'users-permissions': {
+      config: {
+        jwtSecret,
+      },
     },
-  },
-});
+  };
+};
