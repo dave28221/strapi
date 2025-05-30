0eb0466eaefe01b548c2a7da8a98f0c619bea462
David <65214573+dave28221@users.noreply.github.com>
2025-05-30 13:50:47 +0800

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,10 +1,13 @@
 module.exports = ({ env }) => {
-  console.log('Server Port:', env.int('PORT', 1338));  // Add this line to debug
-  return {  
+  console.log('Server Port:', env.int('PORT', 1338));
+
+  const appKeys = env.array('APP_KEYS', ['defaultKey1', 'defaultKey2']); // Fallback keys
+
+  return {
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1338),
     app: {
-      keys: env.array('APP_KEYS'),
+      keys: appKeys,
     },
     webhooks: {
       populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
