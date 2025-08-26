c2238399a5e1e4bc6f8ffeedc71c91b8e1a3e969
David <65214573+dave28221@users.noreply.github.com>
2025-08-26 10:40:14 +0800

    Update server.js

--- config/server.js
+++ config/server.js
@@ -4,7 +4,8 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL', 'http://localhost:1337'), 
+  url: env('PUBLIC_URL', 'http://localhost:1337'),
+  webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
