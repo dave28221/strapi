1b1220864bcc21473bea56af980d9aedf5c31f0e
David <65214573+dave28221@users.noreply.github.com>
2025-08-26 12:20:02 +0800

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,10 +1,12 @@
+// ./config/server.js
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://strapi.liftlegal.com'), // Make sure this matches your .env
+  proxy: env.bool('IS_PROXIED', true), // Tells Strapi it is behind a proxy
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL', 'http://localhost:1337'),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
